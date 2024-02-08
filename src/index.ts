34d41a32825c8d0c195f96fb571bee980f35b17d
PajamaDrive <pajamadrive49@gmail.com>
2024-02-08 23:40:48 +0900

    [fix] ポート番号を修正

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { createServer } from 'http';
 import { connectSocket } from './utils/socket.js';
 
 const app = express();
-const port = 3001;
+const port = 3000;
 const server = createServer(app);
 
 connectSocket(server);
